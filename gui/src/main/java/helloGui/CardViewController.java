ae9864fd8520c01c80b174ab5ee4b035f8994fed
ALEXECKERT93 <78118345+ALEXECKERT93@users.noreply.github.com>
2021-03-24 05:17:11 -0700

    added fmxl file

---
+++ gui/src/main/java/helloGui/CardViewController.java
@@ -0,0 +1,22 @@
+package helloGui;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+public class CardViewController {
+    private static Stage guiStage;
+
+    public static Stage getStage() {
+        return guiStage;
+    }
+
+
+    public void start(Stage primaryStage) {
+        guiStage = primaryStage;
+        // other app initialization logic . . .
+    }
+    public void MC(){}
+}
