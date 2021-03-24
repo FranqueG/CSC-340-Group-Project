10b2cbe8701423f3ea650ad40375b21c1bf91e52
Josh <jmillikan12345@gmail.com>
2021-03-24 13:06:04 -0400

    Fixed javaFx (for real this time)

--- gui/src/main/java/helloGui/Example.java
+++ gui/src/main/java/helloGui/Example.java
@@ -1,12 +1,18 @@
 package helloGui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Example extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        var root = (AnchorPane) FXMLLoader.load(getClass().getResource("/CardView.fxml"));
+        var scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
 }
