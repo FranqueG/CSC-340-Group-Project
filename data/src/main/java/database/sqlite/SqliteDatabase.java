58e39346c2551ed7a0252002d886f9e121ec054f
Josh <jmillikan12345@gmail.com>
2021-03-23 23:32:41 -0400

    fixed typo

--- data/src/main/java/database/sqlite/SqliteDatabase.java
+++ data/src/main/java/database/sqlite/SqliteDatabase.java
@@ -19,7 +19,7 @@ import java.util.Map;
 
 public class SqliteDatabase extends Database {
     private final Connection connection;
-    private static Map<String, String> queryCache = new HashMap();
+    private static Map<String, String> queryCache = new HashMap<>();
 
     public SqliteDatabase(String _filepath) {
         super();
