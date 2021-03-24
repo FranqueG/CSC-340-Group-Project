8e7fb53adbac65505c18d25e50a606c485877f94
ALEXECKERT93 <78118345+ALEXECKERT93@users.noreply.github.com>
2021-03-24 08:30:47 -0700

    added fmxl file

--- app/src/main/java/app/Main.java
+++ app/src/main/java/app/Main.java
@@ -1,8 +1,8 @@
 
 package app;
 import helloGui.CardViewController;
-requires javafx.stage.Stage;
-import javafx.stage.Stage;
+
+
 import static helloApi.HelloApi.helloApi;
 import static helloGui.HelloGui.helloGui;
 public class Main {
