a86785bc0a3fc7b336cd844d0cda4273e1364540
Dominic Turmenne <dominic.l.turmenne@gmail.com>
2021-04-30 15:23:10 -0400

    Made connect() function to streamline http connections

--- api/src/main/java/helloApi/HelloApi.java
+++ api/src/main/java/helloApi/HelloApi.java
@@ -9,17 +9,16 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+
 public class HelloApi {
     public static void helloApi() {
         System.out.println("Hello from api");
-
-
     }
 
     public static String baseURL = "https://api.scryfall.com";
 
     /**
-     * This method
+     * This method separates a string by spaces into words and inserts '+' between them
      *
      * @param _inputStr
      * @param _outputStr
@@ -58,7 +57,7 @@ public class HelloApi {
     }
 
     /**
-     * This method gets all of the available card types for the user to filter by
+     * This method gets all of the available card types for the user to filter
      *
      * @return
      */
@@ -68,7 +67,6 @@ public class HelloApi {
 
         // This string will start out as "creature - " and append the type name to the end
         String baseCardType = "";
-        String specificType = "";
         String callAction = "/catalog";
         String finalAction = "";
 
@@ -109,47 +107,38 @@ public class HelloApi {
 
             // Construct url
             String urlString = baseURL + callAction + finalAction;
-            URL url;
-            try {
-                // Make the connection.
-                url = new URL(urlString);
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod("GET");
-                // Examine response code.
-                int responseCode = connection.getResponseCode();
-                if (responseCode != 200) {
-                    System.out.println("Error: Could not load card information: " + responseCode);
-                } else {
-                    // Parsing input stream into a text string.
-                    BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                    String inputLine;
-                    StringBuffer content = new StringBuffer();
-                    while ((inputLine = input.readLine()) != null) {
-                        content.append(inputLine);
-                    }
-                    // Close connections.
-                    input.close();
-                    connection.disconnect();
-                    // Parse into JSON object.
-                    JSONObject json = new JSONObject(content.toString());
-                    // Put json data into the cardType array list
-                    JSONArray array = json.getJSONArray("data");
-                    for (int j = 0; j < array.length(); j++) {
-                        specificType = baseCardType + array.getString(j);
-                        types.add(specificType);
-                    }
+            try{
+                // Parse into JSON object.
+                JSONObject json = new JSONObject(connect(urlString));
+                // Put json data into the cardType array list
+                JSONArray array = json.getJSONArray("data");
+                for (int j = 0; j < array.length(); j++) {
+                    String specificType = baseCardType + array.getString(j);
+                    types.add(specificType);
                 }
             } catch (Exception exception) {
                 System.out.println("Error: " + exception);
                 return null;
             }
         }
-        // Return the list of card types
         return types;
     }
 
     /**
-     * TODO: Possibly update the color string input (GRWUB format to booleans)
+     * Returns the list of all rulesets for the user to select from
+     * @return
+     */
+    public static ArrayList getRuleSets() {
+        ArrayList<String> rulesets = new ArrayList<>();
+        rulesets.add("Standard");
+        rulesets.add("Modern");
+        rulesets.add("Legacy");
+        rulesets.add("Commander");
+
+        return rulesets;
+    }
+
+    /**
      * Constructs a url string based on the user's search preferences
      *
      * @param _name
@@ -295,17 +284,13 @@ public class HelloApi {
     }
 
     /**
-     * TODO: Update how this method obtains data from the api (Lines 345-380)
-     *
      * This method is for requesting json data and putting it into an array
      * list. It will iterate through every available page of the specific query
      * (page results are limited to 175 entries each)
-     *
-     * @param _cards
      * @param _urlString
-     * @return
+     * @param _cards
      */
-    private static void getCardList(String _urlString, ArrayList<Card> _cards) {
+    private static void getCardList(String _urlString, ArrayList _cards) {
         final String IMAGE_SIZE = "normal";
 
         // Data used to control looping mechanism
@@ -315,93 +300,106 @@ public class HelloApi {
 
         // Do-While loop to get all search content to be sorted
         do {
-            try {
-                // Make the connection.
-                url = new URL(nextPage);
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod("GET");
+            try{
+                // Parse into JSON object.
+                JSONObject json = new JSONObject(connect(_urlString));
+                JSONArray array = json.getJSONArray("data");
+                // Put json data into array list of card objects
+                for (int i = 0; i < array.length(); i++) {
+                    // Get card data on consistent fields
+                    String id = array.getJSONObject(i).getString("id");
+                    String cardName = array.getJSONObject(i).getString("name");
+                    String cardRarity = array.getJSONObject(i).getString("rarity");
+                    String cardType = array.getJSONObject(i).getString("type_line");
 
-                // Examine response code.
-                int responseCode = connection.getResponseCode();
+                    // Check if image uri exists
+                    String imageURI = "";
+                    if (array.getJSONObject(i).has("image_uris")) {
+                        imageURI = array.getJSONObject(i).getJSONObject("image_uris").getString(IMAGE_SIZE);
+                    }
 
-                if (responseCode != 200) {
-                    if (responseCode == 404) {
-                        System.out.println("Error: No cards found: " + responseCode);
+                    // Check if mana_cost field exists since some cards are manaless.
+                    String manaCost = "";
+                    if (array.getJSONObject(i).has("mana_cost")) {
+                        manaCost = array.getJSONObject(i).getString("mana_cost");
                     } else {
-                        System.out.println("Error: Could not load card information: " + responseCode);
+                        manaCost = "0";
                     }
-                } else {
-                    // Parsing input stream into a text string.
-                    BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                    String inputLine;
-                    StringBuffer content = new StringBuffer();
-                    while ((inputLine = input.readLine()) != null) {
-                        content.append(inputLine);
+
+                    // Check if converted mana cost field exists
+                    int convertedManaCost;
+                    if (array.getJSONObject(i).has("cmc")) {
+                        convertedManaCost = array.getJSONObject(i).getInt("cmc");
+                    } else {
+                        convertedManaCost = 0;
                     }
-                    // Close connections.
-                    input.close();
-                    connection.disconnect();
-                    // Print out JSON string.
-                    //System.out.println("Output: " + content.toString());
-                    // Parse into JSON object.
-                    JSONObject json = new JSONObject(content.toString());
 
-                    // Put json data into array list of card objects
-                    JSONArray array = json.getJSONArray("data");
-                    for (int i = 0; i < array.length(); i++) {
-                        // Get card data on consistent fields
-                        String id = array.getJSONObject(i).getString("id");
-                        String cardName = array.getJSONObject(i).getString("name");
-                        String cardRarity = array.getJSONObject(i).getString("rarity");
-                        String cardType = array.getJSONObject(i).getString("type_line");
-
-                        // Check if image uri exists
-                        String imageURI = "";
-                        if (array.getJSONObject(i).has("image_uris")) {
-                            imageURI = array.getJSONObject(i).getJSONObject("image_uris").getString(IMAGE_SIZE);
-                        }
-
-                        // Check if mana_cost field exists since some cards are manaless.
-                        String manaCost = "";
-                        if (array.getJSONObject(i).has("mana_cost")) {
-                            manaCost = array.getJSONObject(i).getString("mana_cost");
-                        } else {
-                            manaCost = "0";
-                        }
-
-                        // Check if converted mana cost field exists
-                        int convertedManaCost;
-                        if (array.getJSONObject(i).has("cmc")) {
-                            convertedManaCost = array.getJSONObject(i).getInt("cmc");
-                        } else {
-                            convertedManaCost = 0;
-                        }
-
-                        // Check if description text exists since some cards have no description.
-                        String cardText = "";
-                        if (array.getJSONObject(i).has("oracle_text")) {
-                            cardText = array.getJSONObject(i).getString("oracle_text");
-                        } else {
-                            cardText = "No description";
-                        }
-
-                        // Add card object to array list
-                        _cards.add(new Card(id, cardName, cardType, manaCost, convertedManaCost, cardText, cardRarity, imageURI));
-
-                        // Get the page link for the next request if it exists
-                        if (json.has("next_page")) {
-                            nextPage = json.getString("next_page");
-                        }
-                        // Get next_page condition
-                        hasNext = json.getBoolean("has_more");
+                    // Check if description text exists since some cards have no description.
+                    String cardText = "";
+                    if (array.getJSONObject(i).has("oracle_text")) {
+                        cardText = array.getJSONObject(i).getString("oracle_text");
+                    } else {
+                        cardText = "No description";
                     }
+
+                    // Add card object to array list
+                    _cards.add(new Card(id, cardName, cardType, manaCost, convertedManaCost, cardText, cardRarity, imageURI));
+
+                    // Get the page link for the next request if it exists
+                    if (json.has("next_page")) {
+                        nextPage = json.getString("next_page");
+                    }
+                    // Get next_page condition
+                    hasNext = json.getBoolean("has_more");
                 }
             } catch (Exception exception) {
                 System.out.println("Error: " + exception);
             }
-            // Continue to next page if it exists
+
         } while (hasNext);
     }
 
+    /**
+     * This method makes an api request and returns the JSON string for other methods to create JSON Objects from
+     * @param _urlString
+     * @return
+     */
+    private static String connect(String _urlString){
+        String jsonString = "";
+
+        URL url;
+        try {
+            // Make the connection.
+            url = new URL(_urlString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            // Examine response code.
+            int responseCode = connection.getResponseCode();
+            if (responseCode != 200) {
+                if (responseCode == 404) {
+                    System.out.println("Error: No data found from request: " + responseCode);
+                } else {
+                    System.out.println("Error: Could not load information: " + responseCode);
+                }
+            } else {
+                // Parsing input stream into a text string.
+                BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuffer content = new StringBuffer();
+                while ((inputLine = input.readLine()) != null) {
+                    content.append(inputLine);
+                }
+                // Close connections.
+                input.close();
+                connection.disconnect();
+                // Parse into JSON object.
+                jsonString = content.toString();
+            }
+        } catch (Exception exception) {
+            System.out.println("Error: " + exception);
+            return null;
+        }
+        return jsonString;
+    }
 
 }
