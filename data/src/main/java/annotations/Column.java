e94ca122c735a2b232fd67f9398d27c23fe77313
Josh <jmillikan12345@gmail.com>
2021-04-30 00:50:04 -0400

    Fixed deck

--- data/src/main/java/annotations/Column.java
+++ data/src/main/java/annotations/Column.java
@@ -1,5 +1,10 @@
 package annotations;
-
+/*
+ * Last updated: 4/27/2021
+ * This Annotation marks a field as representing a column in the database
+ * All fields this is applied to must inherit from Object, eg int is not valid, Integer is.
+ * Authors: Joshua Millikan
+ */
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
--- data/src/main/java/annotations/Table.java
+++ data/src/main/java/annotations/Table.java
@@ -1,4 +1,9 @@
 package annotations;
+/*
+ * Last updated: 4/27/2021
+ * This Annotation marks a class as representing a table in the database
+ * Authors: Joshua Millikan
+ */
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
